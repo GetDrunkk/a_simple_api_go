c32460bb057254e528fcc973e3ae53dacfeb27e1
unknown <w475477941@gmail.com>
2023-01-07 22:13:58 +0900

    step_1_complete

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		response, err := http.Get("https://geoapi.heartrails.com/api/json?method=searchByPostal&postal=5016121")
+		if err != nil || response.StatusCode != http.StatusOK {
+			c.Status(http.StatusServiceUnavailable)
+			return
+		}
+		defer response.Body.Close()
+		b, _ := io.ReadAll(response.Body) //read the json part of the reponse
+		var res map[string]interface{}
+		json.Unmarshal(b, &res)
+		//fmt.Print(res["response"])  check the result
+		c.JSON(200, res)
 	})
-	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+
+	router.Run(":8080")
 }
